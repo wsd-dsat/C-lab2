7077797aaa72a1192a34932d0ed7d63d1c6e93bb
wsd-dsat <78375117+wsd-dsat@users.noreply.github.com>
2022-05-17 12:38:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab{
+    class Program{
+        static void Main(string[] args){
+
+            //lab1
+            int a = 3680;
+
+            int h = a /3600;
+            int x = a /3600;
+            int m = a - h*3600;
+            int s = m%60;
+            Console.WriteLine($"З початку доби пройшло {h} годин {x} хвилин {s} секунд");
+
+
+
+
+        }
+    }
+}
+
+
